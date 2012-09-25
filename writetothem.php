2e1c12e2af5f42fd5cc07ff58a9539f3d30cbbbd
Philip John <phil@philipjohn.co.uk>
2012-09-25 18:18:27 +0100

    Added license

--- writetothem.php
+++ writetothem.php
@@ -1,1 +1,29 @@
 <?php
+/*
+Plugin Name: WriteToThem Widget
+Plugin URI: http://philipjohn.co.uk/category/plugins/writetothem/
+Description: A widget which provides an entry point for mySociety's WriteToThem.com, derived from the original plugin by Richard Pope
+Author: Philip John & Richard Pope
+Version: 2.1
+Author URI: http://philipjohn.co.uk
+License: GPLv3
+*/
+/*  Copyright 2012  Philip John Ltd  (email : talkto@philipjohn.co.uk)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+?>
